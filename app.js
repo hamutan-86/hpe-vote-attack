bc4819964c245857904a2e4f69e65ef537e19d52
hamutan-86 <158189349+hamutan-86@users.noreply.github.com>
2024-07-22 12:40:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-// import express from 'express';
-const express = require("express");
-const fs = require('fs');
+const express = require('express');
 
 const app = express();
 app.use(express.json())
@@ -9,81 +7,29 @@ app.use(express.urlencoded({ extended: true}))
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 app.get('/', (req, res) => {
-  res.send('Piennu Feeling API');
+  res.send('ahhahaga');
 });
 
-app.get('/getCount', (req, res) => {
-  fetch("https://object.piennu777.jp/feelings/api/getcount.php", {method: "GET", headers: {
-    "accept": "*/*",
-    "origin": "https://object.piennu777.jp",
-    "referer": "https://object.piennu777.jp/feelings/",
-    "accept-encoding": "gzip, deflate, br",
-    "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Mobile/15E148 Safari/604.1",
-    "content-length": "0",
-    "accept-language": "en-US,en;q=0.9"
-  }},).then(data => data.text()).then((resp) => {
-    res.status(200).send(resp);
-  })
-});
-
-app.post('/increment/createTask', async (req, res) => {
-  if (!req.body.amount){
-    res.status(400).json({"error": "Form body Invalid"})
-    return false;
-  } else if (typeof req.body.amount != "number"){
-    res.status(400).json({"error": "Form body Invalid"})
-    return false;
+app.post('/attack', async (req, res) => {
+  if ( req.body.amount > 360 ){
+    res.status(400).send("360以上やりたいなら自分でコード書いてやれハゲ")
   }
+  const id = req.body.id;
+  const voteType = req.body.voteType;
   let count = 0;
-  var taskId = Math.floor(Math.random()* 100000000)
-  let jsonObject = JSON.parse(fs.readFileSync('./tasks.json', 'utf8'));
-  jsonObject[taskId.toString()] = `pending:0/${req.body.amount}`;
-  fs.writeFileSync('./tasks.json', JSON.stringify(jsonObject));
-  res.status(200).json({"taskId": taskId})
-  while (true) {
-    await sleep(1);
-    fetch("https://object.piennu777.jp/feelings/api/increment.php", {method: "POST", headers: {
-        "accept": "*/*",
-        "origin": "https://object.piennu777.jp",
-        "referer": "https://object.piennu777.jp/feelings/",
-        "accept-encoding": "gzip, deflate, br",
+  res.status(200).send("started");
+  while ( true ) {
+    await sleep(38);
+    fetch(`https://healthy-person-emulator.org/archives/${id}?_data=routes%2F_layout.archives.%24postId`, {method: "POST", headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
         "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Mobile/15E148 Safari/604.1",
-        "content-length": "0",
-        "accept-language": "en-US,en;q=0.9"
-      }},).then((data) => {})
-    count++
-    if (count == req.body.amount){
+      }, body: `postId=${Number(id)}&voteType=${voteType}&action=votePost`},).then((data) => {
+      count++;
+    })
+    if ( count >= req.body.amount ){
       break;
     }
-    jsonObject[taskId.toString()] = `pending:${count}/${req.body.amount}`;
-    fs.writeFileSync('./tasks.json', JSON.stringify(jsonObject));
   }
-  jsonObject[taskId.toString()] = `success:${count}/${req.body.amount}`;
-  fs.writeFileSync('./tasks.json', JSON.stringify(jsonObject));
-  return true;
-});
-
-app.post('/increment/getTaskResult', (req, res) => {
-  if (!req.body.taskId){
-    res.status(400).json({"error": "Form body Invalid"})
-    return false;
-  } else if (typeof req.body.taskId != "number" && typeof req.body.taskId != "string"){
-    res.status(400).json({"error": "Form body Invalid"})
-    return false;
-  }
-  let taskId = req.body.taskId;
-  if (typeof taskId === "number"){
-    taskId = taskId.toString();
-  }
-  let jsonObject = JSON.parse(fs.readFileSync('./tasks.json', 'utf8'));
-  for (var obj in jsonObject){
-    if (obj === taskId){
-      res.status(200).json({"status": jsonObject[taskId]})
-      return true;
-    }
-  }
-  res.status(400).json({"error": "Task not exists"})
-  return false;
 });
 
 app.listen(3000, () => {
